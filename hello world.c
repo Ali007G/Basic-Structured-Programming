77baef2ab0754b617771134f2d17ec46bf3f5d2c
Ali <95523475+Ali007G@users.noreply.github.com>
2026-02-06 12:32:47 +0600

    Add files via upload

---
+++ hello world.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+
+    printf("Hello World\n");  // \n - new line
+    printf("Course Title: Structured Programming Language Lab\n");
+    printf("Course Code: CSE 0613-114\n");
+
+   // printf("Course Code: CSE 0613-114\n"); single line comment
+
+    /*
+    ----- multi line comment -----
+    printf("Course Title: Structured Programming Language Lab\n");
+    printf("Course Code: CSE 0613-114\n");
+    printf("Course Title: Structured Programming Language Lab\n");
+    printf("Course Code: CSE 0613-114\n");
+    printf("Course Title: Structured Programming Language Lab\n");
+    printf("Course Code: CSE 0613-114\n");
+    */
+    return 0;
+}
