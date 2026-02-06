5a82ea81256e05f8a7aca22aa8537b6a76e6e3b1
Ali <95523475+Ali007G@users.noreply.github.com>
2026-02-06 12:40:09 +0600

    Add files via upload

---
+++ Lab/second.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    char ch;
+    double d;
+    a=10, b=10.12342, ch='A', d=134.5678455;
+    printf("value of a is: %d\n", a);
+    printf("value of b is: %.2f\n", b);
+    printf("value of ch is: %c", ch);
+     printf("value of b is: %lf\n", d);
+    return 0;
+}
