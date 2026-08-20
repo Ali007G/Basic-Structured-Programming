e1a59fc1d6595c8563e441a3f8614a45d9129865
Ali <95523475+Ali007G@users.noreply.github.com>
2026-08-20 14:18:45 +0600

    Add files via upload

---
+++ Lab 03/1_1.c
@@ -0,0 +1,22 @@
+///Area = (√3 / 4) × a × a
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float a, area; /// int a = 10;
+    //printf("Enter the first number: ");
+    scanf("%f", &a);
+
+    area = (sqrt(3)/4) * a * a;
+
+    printf("%.2f", area); // 32
+
+}
+/*
+add = num1 + num2
+sub = num1 - num2
+div = num1/num2
+
+printf("%")
+*/
---
+++ Lab 03/1_2.c
@@ -0,0 +1,14 @@
+/// area of circle = pi * r * r
+#include <stdio.h>
+
+int main()
+{
+    float radius = 7.0;
+    float pi = 3.1416;
+    float area = pi * radius * radius;
+
+    printf("Radius : %.1f\n", radius);
+    printf("Area   : %.2f\n", area)
+
+    return 0;
+}
